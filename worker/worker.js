1408c82cb34228fe42044017c65d250edfbb38ee
Henry <hlewis@uchicago.edu>
2016-07-21 19:29:12 -1000

    I'm learning a lot about promises today

--- worker/worker.js
+++ worker/worker.js
@@ -65,40 +65,34 @@ var compareVideoWithAccounts = function (params) {
     var beginTimeUnix = beginTime.format('x');
     var endTimeUnix = beginTime.add(video.length, 'seconds').format('x');
 
-    return new Promise(function (resolve, reject) {
-        var count = 0;
-        function incr() {
-            count++;
-            if (count === accounts.length) {
-                resolve();
-            }
-        }
+    var promises = [];
 
-        accounts.forEach(function (account) {
-            RiotGames.getMatches(account, {
-                beginTime: beginTimeUnix,
-                endTime: endTimeUnix
-            }).then(function (matches) {
-                var matchCount = 0;
-                function matchIncr() {
-                    matchCount++;
-                    if (matchCount === matches.matches.length) {
-                        incr();
-                    }
-                }
+    accounts.forEach(function (account) {
+        var promise = RiotGames.getMatches(account, {
+            beginTime: beginTimeUnix,
+            endTime: endTimeUnix
+        }).then(function (matches) {
+            var promises = [];
+            if (matches.matches) {
                 matches.matches.forEach(function (match) {
                     var timestamp = moment(match.timestamp).subtract(beginTimeUnix).format('x');
                     var video_url = Twitch.constructURL(video.url, timestamp);
-                    saveMatch({
+                    var promise = saveMatch({
                         match: match,
                         video_url: video_url,
                         account: account,
                         channelID: channelID
-                    }).then(matchIncr).catch(matchIncr);
+                    });
+                    promises.push(promise);
                 });
-            }).catch(incr);
-        })
+            }
+            return Promise.all(promises);
+        });
+
+        promises.push(promise);
     });
+
+    return Promise.all(promises);
 }
 
 // Iterate through given channel's videos in search of new matches
@@ -107,74 +101,60 @@ var iterateVideos = function (params) {
         last_video_id = params.last_video_id,
         accounts = params.accounts;
 
-    return new Promise(function (resolve, reject) {
-        getVideos(channelID).then(function (videos) {
-            var count = 0;
+    return getVideos(channelID).then(function (videos) {
+        var promises = [];
 
-            if (videos.videos.length > 0 &&
-                videos.videos[0].status !== "recording") { // Not currently live
-                updateLastVideo(channelID, videos.videos[0]._id);
+        if (videos.videos.length > 0 &&
+            videos.videos[0].status !== "recording") { // Not currently live
+            updateLastVideo(channelID, videos.videos[0]._id);
+        }
+
+        for (var i = 0; i < videos.videos.length; i++) {
+            if (videos.videos[i]._id === last_video_id) {
+                break;
             }
 
-            for (var i = 0; i < videos.videos.length; i++) {
-                if (videos.videos[i]._id === last_video_id) {
-                    break;
-                }
-
-                if (videos.videos[i].broadcast_type !== "archive") { //TODO: check this
-                    continue;
-                }
-                count++;
-                compareVideoWithAccounts({
-                    video: videos.videos[i],
-                    accounts: accounts,
-                    channelID: channelID
-                }).then(function () {
-                    count--;
-                    if (count === 0){
-                        resolve();
-                    }
-                });
+            if (videos.videos[i].broadcast_type !== "archive") { //TODO: check this
+                continue;
             }
+            var promise = compareVideoWithAccounts({
+                video: videos.videos[i],
+                accounts: accounts,
+                channelID: channelID
+            });
+            promises.push(promise);
+        }
 
-            if (count === 0){
-                resolve();
-            }
-        }).catch(resolve);
-    })
+        return Promise.all(promises);
+    });
 }
 
 // Iterate through stored channels for new matches
 var crawlForNewMatches = function () {
-    return new Promise(function (resolve, reject) {
-        dbConn.getChannels().then(function (channels) {
-            var channelIDs = Object.keys(channels);
-            var count = 0;
-            if (channelIDs.length === 0) {
-                resolve();
-            }
-            function incr() {
-                count++;
-                if (count === channelIDs.length){
-                    resolve();
-                }
-            }
-            channelIDs.forEach(function (channelID) {
-                var channel = channels[channelID];
-                getAccountsByChannel(channel).then(function (accounts) {
-                    iterateVideos({
-                        channelID: channelID,
-                        last_video_id: channel.last_video_id,
-                        accounts: accounts,
-                    }).then(incr);
-                }).catch(incr);
+    return dbConn.getChannels().then(function (channels) {
+        var channelIDs = Object.keys(channels);
+        var promises = [];
+        channelIDs.forEach(function (channelID) {
+            var channel = channels[channelID];
+            var promise = getAccountsByChannel(channel).then(function (accounts) {
+                return iterateVideos({
+                    channelID: channelID,
+                    last_video_id: channel.last_video_id,
+                    accounts: accounts,
+                });
             });
-        }).catch(resolve);
+            promises.push(promise);
+        });
+        return Promise.all(promises);
     });
 }
 
 crawlForNewMatches().then(function () {
     console.log("done");
+}).catch(function (error) {
+    console.log("failed");
+    console.log(error.stack);
+}).then(function () {
     dbConn.close();
     process.exit();
 });
