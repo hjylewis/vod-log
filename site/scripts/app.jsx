09c9e11c47e100e73220eb94a10bbb4a9727be06
Henry <hlewis@uchicago.edu>
2016-08-09 21:05:40 -0400

    basic structure

--- site/scripts/app.jsx
+++ site/scripts/app.jsx
@@ -8,17 +8,72 @@ var GameSummary = React.createClass({
   }
 });
 
-var GameLog = React.createClass({
+var GameLogHead = React.createClass({
   render: function() {
     return (
-        <div className="game-log">
-            {[<GameSummary />, <GameSummary />]}
+        <div className="game-log-head">
+            Log head
         </div>
     );
   }
 });
 
+var GameLog = React.createClass({
+  render: function() {
+    return (
+        <div className="game-log">
+            <GameLogHead />
+            <div className="game-log-body">
+                {[<GameSummary />, <GameSummary />]}
+            </div>
+        </div>
+    );
+  }
+});
+
+var Logo = React.createClass({
+  render: function() {
+    return (
+        <span className="logo">VOD-LOG</span>
+    );
+  }
+});
+
+var Search = React.createClass({
+  render: function() {
+    return (
+        <span className="search">Search</span>
+    );
+  }
+});
+
+var Header = React.createClass({
+  render: function() {
+    return (
+        <div className="header">
+            <Logo />
+            <Search />
+        </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+        <div>
+            <Header />
+            <div className="main">
+                <GameLog />
+            </div>
+        </div>
+    );
+  }
+});
+
+
+
 ReactDOM.render(
-  <GameLog />,
-  document.getElementById('game-log')
+  <App />,
+  document.getElementById('app')
 );
