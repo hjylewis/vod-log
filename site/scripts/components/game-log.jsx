eceb628109c263a3a38e3d94f677e435198fc837
Henry <hlewis@uchicago.edu>
2016-08-10 20:21:58 -0400

    display more stats

--- site/scripts/components/game-log.jsx
+++ site/scripts/components/game-log.jsx
@@ -5,6 +5,7 @@ var ChampionImage = React.createClass({
         )
     }
 });
+
 var SummonerSpells = React.createClass({
     render: function () {
         return (
@@ -12,6 +13,14 @@ var SummonerSpells = React.createClass({
                 <div className={this.props.spell1}/>
                 <div className={this.props.spell2}/>
             </div>
+        )
+    }
+});
+
+var KeystoneImage = React.createClass({
+    render: function () {
+        return (
+            <img className="keystone-image" src={this.props.image}></img>
         )
     }
 });
@@ -28,19 +37,28 @@ var GameSummary = React.createClass({
         var match_data = this.props.data.match_data;
         var player_data = match_data.player_data;
 
+        var kda = `${player_data.stats.kills}/${player_data.stats.deaths}/${player_data.stats.assists}`;
         var duration = Math.floor(match_data.duration / 60);
         var creation = moment(match_data.creation).fromNow();
         var patch = /^[0-9]+\.[0-9]+/.exec(match_data.matchVersion);
+        var lane = player_data.lane;
+        if (lane === "BOTTOM") {
+            lane = player_data.role.replace('DUO_', '');
+        }
+        lane = lane.charAt(0).toUpperCase() + lane.substr(1).toLowerCase();
 
         var outcome = match_data.win ? 'win' : 'loss';
         var classes = `game-summary ${outcome}`;
         return (
             <div onClick={this.onClick} className={classes}>
                 <div className="summary-image">
-                    <ChampionImage image={player_data.champion.image}/>
+                    <ChampionImage image={player_data.champion.image} />
                 </div>
-                <SummonerSpells spell1={player_data.spell1} spell2={player_data.spell2}/>
+                <SummonerSpells spell1={player_data.spell1} spell2={player_data.spell2} />
+                <KeystoneImage image={player_data.keystone.image} />
                 <div className="summary-detail">
+                    <p>Lane: {lane}</p>
+                    <p>KDA: {kda}</p>
                     <p>Duration: {duration}m</p>
                     <p>Creation: {creation}</p>
                     <p>Patch: {patch}</p>
