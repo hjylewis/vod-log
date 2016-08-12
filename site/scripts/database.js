8f729be3d92234140bdf7c8d426ced14f65597b9
Henry <hlewis@uchicago.edu>
2016-08-12 17:15:45 -0400

    start on database wrapper

---
+++ site/scripts/database.js
@@ -0,0 +1,35 @@
+(function() {
+    var db = window.db = this;
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyChH058iEwAz-9eAqL9mN_MGOw7vkILUIk",
+        authDomain: "vod-log.firebaseapp.com",
+        databaseURL: "https://vod-log.firebaseio.com",
+        storageBucket: "vod-log.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+    function _addListener (query) {
+        return query.once('value').then(function(snapshot) {
+            return snapshot.val();
+        });
+    }
+
+    function _orderByCreation (query) {
+        return query.orderByChild('match_data/creation/player_data/championId');
+    }
+
+    function _getData (ref) {
+        return _addListener(_orderByCreation(firebase.database().ref(ref)));
+    }
+
+    function getMatches () {
+        // TODO dev env
+        return _getData('dev/store/matches');
+    }
+
+    getMatches().then(function (value) {
+        console.log(value);
+    });
+})();
