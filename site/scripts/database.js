4874d9005d5e5cca584653ba1cdf35907f4deb3f
Henry <hlewis@uchicago.edu>
2016-08-12 23:09:42 -0400

    flesh out database api

--- site/scripts/database.js
+++ site/scripts/database.js
@@ -1,6 +1,10 @@
 (function() {
     var db = window.db = this;
 
+    // TODO dev env
+    var store = "dev/store";
+
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyChH058iEwAz-9eAqL9mN_MGOw7vkILUIk",
@@ -16,20 +20,83 @@
         });
     }
 
-    function _orderByCreation (query) {
-        return query.orderByChild('channelID').equalTo("meteos").orderByChild('creation').limitToLast(10);
+    function _limit (query, limit) {
+        return query.limitToLast(limit);
     }
 
-    function _getData (ref) {
-        return _addListener(_orderByCreation(firebase.database().ref(ref)));
+    function _orderByCreation (query, orderBy) {
+        return query.orderByChild(orderBy);
     }
 
-    function getMatches () {
-        // TODO dev env
-        return _getData('dev/store/matches');
+    function _getData (ref, params) {
+        var {limit, orderBy} = params;
+        var query = firebase.database().ref(ref);
+
+        if (orderBy) {
+            query = _orderByCreation(query, orderBy);
+        }
+        if (limit) {
+            query = _limit(query, limit);
+        }
+
+        return _addListener(query);
     }
 
-    getMatches().then(function (value) {
+    function _joinMatches (matchIds) {
+        return Promise.forEach(matchIds, function (matchId) {
+            return _addListener(firebase.database().ref(store + '/matches/' + matchId));
+        });
+    }
+
+    db.getMatches = function (params) {
+        var {channel, champion, role} = params;
+        var {limit, orderBy} = params;
+
+        var ref = store + "/matches";
+
+        if (channel) {
+            ref = store + '/channels/' + channel + '/matches'
+        } else if (champion) {
+            ref = store + '/champion/' + champion + '/matches'
+        } else if (role) {
+            ref = store + '/role/' + role + '/matches'
+        }
+
+        return _getData(ref, params).then(function (matches) {
+            var matchIds = Object.keys(matches).reverse();
+            return _joinMatches(matchIds);
+        });
+    };
+
+
+    // Abstractions
+    db.getChannelMatches = function (channel) {
+        return db.getMatches({
+            channel: channel,
+            orderBy: "creation",
+            limit: 10
+        });
+    };
+
+    db.getChampionMatches = function (championId) {
+        return db.getMatches({
+            champion: championId,
+            orderBy: "creation",
+            limit: 10
+        });
+    };
+
+    db.getRoleMatches = function (role) {
+        return db.getMatches({
+            role: role,
+            orderBy: "creation",
+            limit: 10
+        });
+    };
+
+
+    // Usage
+    getChannelMatches("meteos").then(function (value) {
         console.log(value);
     });
 })();
