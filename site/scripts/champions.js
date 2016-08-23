bd1b60352a1f324aa2e36b8c465af85034bafb2f
Henry <hlewis@uchicago.edu>
2016-08-23 01:40:09 -0400

    get champion image

--- site/scripts/champions.js
+++ site/scripts/champions.js
@@ -13,6 +13,11 @@ function champions () {
         return formated;
     });
 
+    var realmEndpoint = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/realm?api_key=RGAPI-2EFFBEA3-3500-443C-934E-1E212B9FE551";
+    var realmPromise = fetch(realmEndpoint).then(function (response) {
+        return response.json();
+    });
+
     champions.get = function (key) {
         return promise.then(function (champions) {
             var stripped = key.toLowerCase().replace(/[^a-zA-z]/g, '');
@@ -31,12 +36,29 @@ function champions () {
         });
     };
 
-    champions.getChampionHead = function (key) {
-        return champions.getById(key).then(function (champion) {
+    champions.getImagePath = function (id) {
+        var endPoint = `https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/${id}?champData=image&api_key=RGAPI-2EFFBEA3-3500-443C-934E-1E212B9FE551`;
+        return fetch(endPoint).then(function (response) {
+            return response.json();
+        }).then(function (response) {
+            return response.image.full;
+        });
+    };
+
+    champions.getImage = function (version, champion) {
+        return `http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion}`;
+    };
+
+    champions.getChampionHead = function (id) {
+        return Promise.all([
+            champions.getById(id),
+            champions.getImagePath(id),
+            realmPromise
+        ]).then(function ([champion, path, realm]) {
             return {
                 type: 'champion',
                 name: champion.name,
-                logo: champion.logo,
+                logo: champions.getImage(realm.dd, path),
             };
         });
     };
