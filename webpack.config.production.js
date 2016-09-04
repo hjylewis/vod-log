0470a1eaf1d6a19348701aad9f79f68193573b98
Henry <hlewis@uchicago.edu>
2016-09-04 16:34:23 -0400

    production build

---
+++ webpack.config.production.js
@@ -0,0 +1,52 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        style: './site/style-index.js',
+        bundle: ['whatwg-fetch', './site/index.js']
+    },
+
+    output: {
+        path: __dirname + '/site/compiled',
+        filename: '[name].js',
+        publicPath: '/compiled'
+    },
+
+    debug: false,
+    devtool: false,
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+            },
+            {
+                test: /\.woff(2)?(\?[a-z0-9]+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.json?$/,
+                loader: "json"
+            }
+        ]
+    }
+};
