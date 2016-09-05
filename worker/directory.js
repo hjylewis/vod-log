f3634114f0667a3807ac39e93ce56ec2d2275458
Henry <hlewis@uchicago.edu>
2016-09-04 23:31:29 -0400

    new accounts

--- worker/directory.js
+++ worker/directory.js
@@ -1,4 +1,5 @@
 var directory = {
+    // Cloud 9
     "c9sneaky": {
         displayName: "Sneaky",
         accounts: [
@@ -19,20 +20,90 @@ var directory = {
             {name: "Lil Bunny FuFuu", region: "NA"},
         ]
     },
-
-    "TSM_Doublelift": {
-        displayName: "Doublelift",
+    "HaiL9": {
+        displayName: "Hai",
         accounts: [
-            {name: "Doublelift", region: "NA"},
+            {name: "C9 Hai", region: "NA"},
         ]
     },
-
+    "Jensen": {
+        displayName: "Jensen",
+        accounts: [
+            {name: "C9 Jensenn", region: "NA"},
+        ]
+    },
     "rush": {
         displayName: "Rush",
         accounts: [
             {name: "Tiltlord", region: "KR"},
         ]
     },
+
+    // TSM
+    "TSM_Doublelift": {
+        displayName: "Doublelift",
+        accounts: [
+            {name: "Doublelift", region: "NA"},
+        ]
+    },
+    "TSM_Bjergsen": {
+        displayName: "Bjergsen",
+        accounts: [
+            {name: "Soerenbjerg", region: "NA"},
+        ]
+    },
+    "TSM_Svenskeren": {
+        displayName: "Svenskeren",
+        accounts: [
+            {name: "BLOCK IT", region: "NA"},
+        ]
+    },
+    "TSM_Dyrus": {
+        displayName: "Dyrus",
+        accounts: [
+            {name: "a very bad time", region: "NA"},
+        ]
+    },
+
+    // CLG
+    "Aphromoo": {
+        displayName: "Aphromoo",
+        accounts: [
+            {name: "aphromoo", region: "NA"},
+        ]
+    },
+    "Zionspartan": {
+        displayName: "Darshan",
+        accounts: [
+            {name: "DarDarBlNKS", region: "NA"},
+        ]
+    },
+    "LOLWhoIsHe": {
+        displayName: "Huhi",
+        accounts: [
+            {name: "Huhi", region: "NA"},
+        ]
+    },
+    "Xmithie": {
+        displayName: "Xmithie",
+        accounts: [
+            {name: "Amberly LFT", region: "NA"},
+        ]
+    },
+    "Stixxay": {
+        displayName: "Stixxay",
+        accounts: [
+            {name: "Stixxay", region: "NA"},
+        ]
+    },
+
+    // Echo Fox
+    "Froggen": {
+        displayName: "Froggen",
+        accounts: [
+            {name: "Anivia Kid", region: "NA"},
+        ]
+    }
 };
 
 module.exports = directory;
