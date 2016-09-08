a60bc51e6bd623de30e56cf68fdf188563522792
Henry <hlewis@uchicago.edu>
2016-09-07 23:20:44 -0400

    replace twitter with a mailchimp newsletter

--- site/scripts/components/footer.js
+++ site/scripts/components/footer.js
@@ -6,7 +6,7 @@ export default React.createClass({
         var year = new Date().getFullYear();
         return (
             <div className="footer">
-                <p><a href="mailto:info@thevodlog.com">Email</a> | <a href="https://twitter.com/hjylewis">Twitter</a> | <a href="https://github.com/hjylewis/vod-log">Github</a></p>
+                <p><a href="mailto:info@thevodlog.com">Email</a> | <a href="http://eepurl.com/ceKySH" target="_blank" rel="nofollow">Subscribe</a> | <a href="https://github.com/hjylewis/vod-log" target="_blank" rel="nofollow">Github</a></p>
                 <p><a href="/suggestion">Help us with missing information</a></p>
                 <p>&#169; {year} vodlog</p>
             </div>
